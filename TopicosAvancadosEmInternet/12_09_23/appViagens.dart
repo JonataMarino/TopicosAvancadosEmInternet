e6c9f263b59a8fb421aa44de597785ad82353a76
jsmar <jsmarino1993@outlook.com>
2023-09-21 19:27:15 -0300

    21_09_23

---
+++ TopicosAvancadosEmInternet/12_09_23/appViagens.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppViagens());
+}
+
+class AppViagens extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(title: Text('App Viagens')),
+            body: Column(
+              children: <Widget>[
+                Image(image: AssetImage('images/ponta-negra.jpeg')),
+                Container(
+                    padding: EdgeInsets.all(20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                              Text('Praia de Ponta Negra',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black)),
+                              Text('Rio Grande do Norte - Brasil',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.grey))
+                            ])),
+                        Row(children: [
+                          Icon(Icons.star, color: Colors.yellow),
+                          Text('Mais  visto na última hora')
+                        ]),
+                      ],
+                    )),
+                Container(
+                    padding: EdgeInsets.all(20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Column(children: [
+                                  Icon(Icons.call, color: Colors.blue),
+                                  Text('Ligar')
+                                ]))),
+                        Expanded(
+                            child: TextButton(
+                                onPressed: () {},
+                                child: Column(children: [
+                                  Icon(Icons.location_on, color: Colors.blue),
+                                  Text('Mapa')
+                                ]))),
+                      ],
+                    )),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextButton(
+                              onPressed: () {}
+                              child: Column (children: [
+                                Icon(Icons.share, color: Colors.blue),
+                                Text('Compartilhar')
+                              ]))),
+                        ],
+                      )),
+                Container(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                        'A Praia de Ponta Negra é uma praia localizada no bairro de mesmo nome em Natal, capital do estado brasileiro do Rio Grande do Norte.'))
+              ],
+            )));
+  }
+}
